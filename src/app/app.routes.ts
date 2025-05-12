532483097b62487fe6a9ee2fd1a1753db800d465
Andrich Barreto <andrich717@gmail.com>
2025-05-12 16:14:02 -0500

    Creacion pages auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,16 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/auth/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'signup',
+    loadComponent: () => import('./pages/auth/signup/signup.page').then( m => m.SignupPage)
+  },
+  {
+    path: 'forgot-password',
+    loadComponent: () => import('./pages/auth/forgot-password/forgot-password.page').then( m => m.ForgotPasswordPage)
+  },
 ];
