d0652680b7ce884e4ba34d2ea9e9645183eff14e
Andrich Barreto <andrich717@gmail.com>
2025-05-12 18:11:46 -0500

    Configuracion del sign-up (html y css)

--- src/app/pages/auth/signup/signup.page.ts
+++ src/app/pages/auth/signup/signup.page.ts
@@ -1,20 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Location } from '@angular/common';
+import { Router, NavigationExtras } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-
+import { IonContent, IonHeader, IonTitle, IonToolbar, 
+  IonItem, 
+  IonInput, 
+  IonButton, 
+  IonIcon, 
+  IonText } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { 
+  personOutline, 
+  mailOutline, 
+  lockClosedOutline 
+} from 'ionicons/icons';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,IonItem,
+    IonInput,
+    IonButton,
+    IonIcon,
+    IonText]
 })
 export class SignupPage implements OnInit {
 
-  constructor() { }
+  constructor(private location: Location, private router: Router) { 
+    addIcons({ personOutline, mailOutline, lockClosedOutline });
+  }
 
   ngOnInit() {
   }
-
+goBack() {
+  if (window.history.length > 1) {
+    this.location.back();
+  } else {
+    this.router.navigate(['/login']); // Necesitarás inyectar Router
+  }
 }
+}
+
