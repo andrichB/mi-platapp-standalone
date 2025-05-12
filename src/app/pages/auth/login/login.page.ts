bd27b6e332b6668074075585f245d755bebe7f60
Andrich Barreto <andrich717@gmail.com>
2025-05-12 17:27:26 -0500

    Configuracion html y css

--- src/app/pages/auth/login/login.page.ts
+++ src/app/pages/auth/login/login.page.ts
@@ -1,20 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar,
+/*Agregados por mi*/  
+  IonItem,
+  IonInput,
+  IonButton,
+  IonText,
+  IonIcon } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonItem,
+    IonInput,
+    IonButton,
+    IonText,
+    IonIcon,]
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+  goToSignup() {
+    this.router.navigate(['/signup']); // Navega a la ruta 'signup'
   }
 
 }
