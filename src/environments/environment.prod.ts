e95e25c072ef5ee5808a4d69f3c5ed783cf8f5a7
Andrich Barreto <andrich717@gmail.com>
2025-05-13 18:57:42 -0500

    Configuracion inicial segun documentacion Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+
+
 export const environment = {
-  production: true
+  production: true,
+ firebaseConfig : {
+  apiKey: "AIzaSyBSEXxy7LcaOEY5xodhMjMLe0bG-_Khges",
+  authDomain: "mi-platapp.firebaseapp.com",
+  projectId: "mi-platapp",
+  storageBucket: "mi-platapp.firebasestorage.app",
+  messagingSenderId: "374225387962",
+  appId: "1:374225387962:web:312f04a6b7ae4855489d83",
+  measurementId: "G-GE8RT0J397"
+}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,18 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+  apiKey: "AIzaSyBSEXxy7LcaOEY5xodhMjMLe0bG-_Khges",
+  authDomain: "mi-platapp.firebaseapp.com",
+  projectId: "mi-platapp",
+  storageBucket: "mi-platapp.firebasestorage.app",
+  messagingSenderId: "374225387962",
+  appId: "1:374225387962:web:312f04a6b7ae4855489d83",
+  measurementId: "G-GE8RT0J397"
+}
 };
 
 /*
