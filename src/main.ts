46d0893b86d3e97768e171642ffa799070718865
Andrich Barreto <andrich717@gmail.com>
2025-05-13 20:31:09 -0500

    modificacion del main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,29 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
+import { 
+  RouteReuseStrategy, 
+  provideRouter, 
+  withPreloading, 
+  PreloadAllModules 
+} from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from './environments/environment';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    // Configuración de Ionic
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
+    
+    // Configuración de Rutas
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    
+    // Configuración de Firebase
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
   ],
-});
+}).catch(err => console.error(err));
