37b6468981bead7c713db9850b801b88ea0f1df2
Andrich Barreto <andrich717@gmail.com>
2025-05-13 21:38:15 -0500

    Modificaicion del css de login

--- src/app/pages/auth/login/login.page.ts
+++ src/app/pages/auth/login/login.page.ts
@@ -9,7 +9,12 @@ import { Router } from '@angular/router';
 import { AuthService } from '../../../services/auth.service'; // Asegúrate de que la ruta sea correcta
 import { ToastService } from '../../../services/toast.service'; // Opcional pero recomendado
 import { firstValueFrom } from 'rxjs';
-
+import { addIcons } from 'ionicons';
+import { 
+  personOutline, 
+  mailOutline, 
+  lockClosedOutline 
+} from 'ionicons/icons';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -26,30 +31,32 @@ export class LoginPage {
   email: string = '';
   password: string = '';
   loading: boolean = false;
-  
+ 
   private authService = inject(AuthService);
   private router = inject(Router);
   private toastService = inject(ToastService); // Opcional
-
+  constructor() {
+    addIcons({ personOutline, mailOutline, lockClosedOutline });
+  }
   // En tu login.page.ts
-async login() {
-  if (!this.email || !this.password) {
-    this.toastService.showError('Por favor complete todos los campos');
-    return;
-  }
+  async login() {
+    if (!this.email || !this.password) {
+      this.toastService.showError('Por favor complete todos los campos');
+      return;
+    }
 
-  this.loading = true;
-  
-  try {
-    await firstValueFrom(this.authService.login(this.email, this.password));
-    // La navegación ahora se maneja en el servicio
-  } catch (error) {
-    // Los errores ya los maneja el AuthService
-    console.error('Error en el componente:', error);
-  } finally {
-    this.loading = false;
+    this.loading = true;
+    
+    try {
+      await firstValueFrom(this.authService.login(this.email, this.password));
+      // La navegación ahora se maneja en el servicio
+    } catch (error) {
+      // Los errores ya los maneja el AuthService
+      console.error('Error en el componente:', error);
+    } finally {
+      this.loading = false;
+    }
   }
-}
 
   goToSignup() {
     this.router.navigate(['/signup']);
